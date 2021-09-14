650f0c0d09245e1b507364ee892299762388742d
TimerOverflow <38400bps@gmail.com>
2021-09-14 21:35:40 +0900

    1. add compiler target "Microchip Studio" 2. convert tab -> space.

--- crc16.h
+++ crc16.h
@@ -5,37 +5,43 @@
 */
 /*********************************************************************************/
 #ifndef __CRC16_H__
-#define	__CRC16_H__
+#define __CRC16_H__
 /*********************************************************************************/
 #include "SysTypedef.h"
 /*********************************************************************************/
-#define AVR_CRC16_REVISION_DATE		20200907
+#define AVR_CRC16_REVISION_DATE    20210604
 /*********************************************************************************/
 /** REVISION HISTORY **/
 /*
-	2020. 09. 07.					- __CRC16_TARGET_IAR_AVR__ 추가. AVR 타겟일 경우 crc테이블을 플래쉬 메모리에 할당.
-	Jeong Hyun Gu
+  2021. 06. 04.          - __CRC16_TARGET_MICROCHIP_STUDIO__ 추가.
+  Jeong Hyun Gu
 
-	2019. 10. 07.					- 'SysTypedef.h' 적용.
-	Jeong Hyun Gu
+  2020. 09. 07.          - __CRC16_TARGET_IAR_AVR__ 추가. AVR 타겟일 경우 crc테이블을 플래쉬 메모리에 할당.
+  Jeong Hyun Gu
 
-	2018. 05. 02.					- CRC16 테이블 변수 타입 변경.
-	Jeong Hyun Gu
+  2019. 10. 07.          - 'SysTypedef.h' 적용.
+  Jeong Hyun Gu
 
-	2016. 11. 08.					- revision valid check 추가.
-	Jeong Hyun Gu
+  2018. 05. 02.          - CRC16 테이블 변수 타입 변경.
+  Jeong Hyun Gu
 
-	2016. 10. 28.					- 초기버전.
-	Jeong Hyun Gu
+  2016. 11. 08.          - revision valid check 추가.
+  Jeong Hyun Gu
+
+  2016. 10. 28.          - 초기버전.
+  Jeong Hyun Gu
 */
 /*********************************************************************************/
 /**Define**/
 
-#define true		1
-#define	false		0
-#define	null		0
+#define true    1
+#define false   0
+#define null    0
 
-#define __CRC16_TARGET_IAR_AVR__			true
+#define  __CRC16_TARGET_UNKNOWN__             0
+#define __CRC16_TARGET_IAR_AVR__              1
+#define __CRC16_TARGET_MICROCHIP_STUDIO__     2
+#define __CRC16_TARGET_COMPILER__             __CRC16_TARGET_IAR_AVR__
 
 /*********************************************************************************/
 /**Enum**/
